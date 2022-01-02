b553c8baebbe7c1497d0d20c288edf3593d405bb
ZhuoranWang <41529680+wang-zhuoran@users.noreply.github.com>
2022-01-02 11:40:14 +0800

    Create TestAbstractFactory.java

---
+++ 代码/TestAbstractFactory.java
@@ -0,0 +1,69 @@
+public class TestAbstractFactory {
+
+    public static void main(String[] args){
+        ConcreteFactory1 concreteFactory1 = new ConcreteFactory1();
+        concreteFactory1.createProductA();
+        concreteFactory1.createProductB();
+
+        ConcreteFactory2 concreteFactory2 = new ConcreteFactory2();
+        concreteFactory2.createProductA();
+        concreteFactory2.createProductB();
+    }
+}
+
+interface AbstractProductA{ //可以认为是A 品牌的产品，为了可扩展性定义为抽象类
+
+}
+
+class ConcreteProductA1 implements AbstractProductA{
+
+}
+
+class ConcreteProductA2 implements AbstractProductA{
+
+}
+
+interface AbstractProductB{
+
+}
+
+class ConcreteProductB1 implements AbstractProductB{
+
+}
+
+class ConcreteProductB2 implements AbstractProductB{
+
+}
+
+interface AbstractFactory{ //抽象工厂类，可以造A品牌的产品，也可以造B品牌的产品
+    public AbstractProductA createProductA();
+    public AbstractProductB createProductB();
+}
+
+class ConcreteFactory1 implements AbstractFactory{
+    @Override
+    public AbstractProductA createProductA(){
+        System.out.println("A品牌的1产品");
+        return new ConcreteProductA1();
+    }
+
+    @Override
+    public AbstractProductB createProductB() {
+        System.out.println("B品牌的1产品");
+        return new ConcreteProductB1();
+    }
+}
+
+class ConcreteFactory2 implements AbstractFactory{
+    @Override
+    public AbstractProductA createProductA() {
+        System.out.println("A品牌的2产品");
+        return new ConcreteProductA2();
+    }
+
+    @Override
+    public AbstractProductB createProductB() {
+        System.out.println("B品牌的2产品");
+        return new ConcreteProductB2();
+    }
+}
